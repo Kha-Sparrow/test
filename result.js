5cb4e528fde89a9bf43f49a9f6418a4f23993b3d
Kha-Sparrow <khaclever@gmail.com>
2024-12-28 13:07:55 +0700

    Update result.js

--- result.js
+++ result.js
@@ -7,11 +7,13 @@ function updatePercent(slider, displayId) {
 
 // Hàm lưu thông tin vào localStorage
 function saveInfo(memberNumber) {
+    const mssv = document.getElementById(`mssv${memberNumber}`).value;
     const name = document.getElementById(`name${memberNumber}`).value;
     const job = document.getElementById(`job${memberNumber}`).value;
     const contribution = document.getElementById(`contribution${memberNumber}`).value;
 
     // Lưu thông tin vào localStorage
+    localStorage.setItem(`mssv${memberNumber}`, mssv);
     localStorage.setItem(`name${memberNumber}`, name);
     localStorage.setItem(`job${memberNumber}`, job);
     localStorage.setItem(`contribution${memberNumber}`, contribution);
@@ -19,10 +21,14 @@ function saveInfo(memberNumber) {
 
 // Hàm tải thông tin từ localStorage khi trang được tải lại
 function loadInfo(memberNumber) {
+    const mssv = localStorage.getItem(`mssv${memberNumber}`);
     const name = localStorage.getItem(`name${memberNumber}`);
     const job = localStorage.getItem(`job${memberNumber}`);
     const contribution = localStorage.getItem(`contribution${memberNumber}`);
 
+    if (mssv) {
+        document.getElementById(`mssv${memberNumber}`).value = mssv;
+    }
     if (name) {
         document.getElementById(`name${memberNumber}`).value = name;
     }
